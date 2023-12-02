63552944ae39b6dfe6db01eb5b047aa092e23480
VProject25 <152671590+VProject25@users.noreply.github.com>
2023-12-02 10:49:07 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Nhi';
+const giftUrl = 'https://www.facebook.com/doan.nhi.5264';
+const eventName = 'Happy Birthday to you, Nhi';
+const titleCard = 'Gửi đến Nhi';
+const contentCard = 'Nhân ngày sinh nhật, chúc Nhi tuổi mới thật nhiều niềm vui, sức khỏe. Wish you all the best!!!';
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
